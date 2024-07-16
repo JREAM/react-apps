b55162ddbac5d456f1f69b15bd28fcd648db0ae8
Jesse Boyer <imboyus@gmail.com>
2024-07-16 05:32:17 -0400

    use hash router again it worked

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 
 import Root from './routes/root'
 import ErrorPage from './error-page'
@@ -14,11 +14,10 @@ import Todo from 'routes/todo'
 
 import './index.css'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Root />,
-
     errorElement: <ErrorPage />,
     children: [
       {
